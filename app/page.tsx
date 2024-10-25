e0290a4f21ab3b6611ba82c84444fd1968f8586c
tahaali-dev <123803932+tahaali-dev@users.noreply.github.com>
2024-10-25 10:26:07 +0530

    added lenis for smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect } from "react";
 import AboutUs from "./(frontendPages)/home/components/AboutUs";
 import Benefits from "./(frontendPages)/home/components/Benefits";
 import CompaniesMarquee from "./(frontendPages)/home/components/CompaniesMarquee";
@@ -8,11 +10,30 @@ import SliderOfLove from "./(frontendPages)/home/components/SliderOfLove";
 import VideoPlayer from "./(frontendPages)/home/components/VideoPlayer";
 import WhatWeDo from "./(frontendPages)/home/components/WhatWeDo";
 import Footer from "./CommonComps/Footer";
-import VerticalSlider from "./CommonComps/TextChanging";
 import EmptyContainer from "./styledComps/containers";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
 // Imports ------
 
 const Homepage = () => {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis();
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", (e) => {
+      // console.log(e);
+    });
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Firstfold />
