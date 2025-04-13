8ee2c47b5882f20b8dad77d68f9c9abeb22eaaaa
tahaali-dev <tahaali.css@gmail.com>
2025-04-13 13:19:01 +0530

    security headers updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+ headers: async () => [
+  {
+   source: '/(.*)',
+   headers: [
+    {
+     key: 'Content-Security-Policy',
+     value: "frame-ancestors 'self'"
+    },
+    {
+     key: 'Permissions-Policy',
+     value: 'camera=(), geolocation=(), microphone=()'
+    },
+    {
+     key: 'Referrer-Policy',
+     value: 'strict-origin-when-cross-origin'
+    },
+    {
+     key: 'X-Frame-Options',
+     value: 'SAMEORIGIN'
+    },
+    {
+     key: 'Strict-Transport-Security',
+     value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+     key: 'X-Content-Type-Options',
+     value: 'nosniff'
+    },
+    {
+     key: 'X-XSS-Protection',
+     value: '1; mode=block'
+    }
+   ]
+  }
+ ]
+};
 
 export default nextConfig;
